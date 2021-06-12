4e309d87f992f3bf4dd67cfa3b546804bab6c960
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-12 03:53:55 -0400

    add : MaximumCheck.java

--- MaximumCheck.java
+++ MaximumCheck.java
@@ -1,8 +1,39 @@
 package com;
 
-public interface MaximumCheck {
+/**
+ * MaximumCheck is an class of public type
+ * @author rekha
+ *
+ */
+public class MaximumCheck {
+	
+	/**
+	 * checkMaximum is a method here
+	 * calculating Maximum of Three Numbers using CompareTo method
+	 *returning int max value
+	 */
+	
+		public static int checkMaximum(Integer x, Integer y, Integer z) {
+			Integer max = x;
+			if(y.compareTo(max) > 0) {
+				max = y; // y is largest now
+			}
+			if(z.compareTo(max) > 0) {
+				max = z; // z is largest now
+			}
+			return max; // returns maximum number
+		}
+		
+		/**
+		 * main method
+		 *calling checkMaximum function 
+		 *printing max value
+		 */
+		
+		public static void main(String[] args) {
+			
+			int c = checkMaximum(10,23,15);
+			System.out.println("The Maximum value is : " +c);
+		}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Find Maximum Problem using Generics");
-	}
 }
